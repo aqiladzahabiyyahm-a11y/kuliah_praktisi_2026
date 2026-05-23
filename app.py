ca83f811f2950920131cf9d53221faf9ebd00a63
aqiladzahabiyyahm-a11y <aqiladzahabiyyahm@gmail.com>
2026-05-23 10:31:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
+st.set_page_config( 
+  page_title="kuliah praktisi 2026",
+  page_icon="🧊",
+  layout="centered",
+  initial_sidebar_state="expended"
+)
+
 st.title (" Dashboard ")
+st.hearder("laporan bulanan")
+st.subharder(" monthly expenses"
+st. caption("made with using streamlit")
+st.write("Hello, *World!* :sunglasses:")  
+             
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
